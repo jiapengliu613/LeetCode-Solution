2e5f4be04ae35d2601af0a18f1520225de8704e5
jiapengliu613 <jiapengliu613@gmail.com>
2016-11-28 21:34:08 -0800

    Solution

---
+++ Algorithm/Roman to Integer.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int romanToInt(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        HashMap<Character, Integer> m = new HashMap<>();
+        m.put('I', 1);
+	    m.put('V', 5);
+	    m.put('X', 10);
+	    m.put('L', 50);
+	    m.put('C', 100);
+	    m.put('D', 500);
+	    m.put('M', 1000);
+	    int n = s.length();
+	    int result = m.get(s.charAt(n - 1));
+	    for (int i = n - 2; i >= 0; i--) {
+	        if (m.get(s.charAt(i)) >=  m.get(s.charAt(i + 1))) {
+	            result += m.get(s.charAt(i));
+	        } else {
+	            result -= m.get(s.charAt(i));
+	        }
+	    }
+	    return result;
+    }
+}
