2414b9b1706161bb703b5abb398605501558c0d2
jiapengliu613 <jiapengliu613@gmail.com>
2016-11-29 10:06:41 -0800

    update

--- Algorithm/Reverse Linked List.java
+++ Algorithm/Reverse Linked List.java
@@ -6,6 +6,7 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+//rcursive
 public class Solution {
     public ListNode reverseList(ListNode head) {
         if (head == null || head.next == null) {
@@ -16,5 +17,32 @@ public class Solution {
         next.next = head;
         head.next = null;
         return newHead;
+    }
+}
+
+//non-recursive
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode fast = head;
+        ListNode slow = null;
+        while (fast != null) {
+            ListNode next = fast.next;
+            fast.next = slow;
+            slow = fast;
+            fast = next;
+        }
+        return slow;
+        
     }
 }
