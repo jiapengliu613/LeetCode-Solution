a15b98d7201305a6cfe4c5fabc05c184893ea502
jiapengliu613 <jiapengliu613@gmail.com>
2016-12-01 20:50:10 -0800

    Solution

---
+++ Algorithm/Fizz Buzz.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> result = new ArrayList<>();
+        if (n <= 0) {
+            return result;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0) {
+                result.add("FizzBuzz");
+            } else if (i % 5 == 0) {
+                result.add("Buzz");
+            } else if (i % 3 == 0) {
+                result.add("Fizz");
+            } else {
+                result.add(String.valueOf(i));
+            }
+        }
+        return result;
+    }
+}
