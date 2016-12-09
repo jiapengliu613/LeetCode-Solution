858011ef461eefc86145821607485f3d61f5ed6a
jiapengliu613 <jiapengliu613@gmail.com>
2016-12-09 14:50:30 -0800

    Solution

---
+++ Algorithm/Power of Three.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public boolean isPowerOfThree(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        if (n == 1) {
+            return true;
+        }
+        while (n > 1) {
+            if (n % 3 != 0) {
+                return false;
+            }
+            n = n / 3;
+        }
+        return true;
+    }
+}
