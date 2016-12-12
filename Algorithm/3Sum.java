0e60a21eb3b60a57fb77bdbc715bcc1c1848aaa4
jiapengliu613 <jiapengliu613@gmail.com>
2016-12-12 10:55:33 -0800

    Solution

---
+++ Algorithm/3Sum.java
@@ -0,0 +1,38 @@
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length < 3) {
+            return result;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1, right = nums.length - 1;
+            int sum = 0 - nums[i];
+            while (left < right) {
+                if (nums[left] + nums[right] == sum) {
+                    List<Integer> sol = new ArrayList<>();
+                    sol.add(nums[i]);
+                    sol.add(nums[left]);
+                    sol.add(nums[right]);
+                    result.add(sol);
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                } else if (nums[left] + nums[right] < sum) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
+}
