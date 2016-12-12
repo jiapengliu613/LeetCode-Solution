a4d901d0e638bab3b19f021ee28fb118dbb67866
jiapengliu613 <jiapengliu613@gmail.com>
2016-12-12 11:56:22 -0800

    Solution

---
+++ Algorithm/Pow(x, n).java
@@ -0,0 +1,14 @@
+public class Solution {
+    public double myPow(double x, int n) {
+        double ans = 1;
+        long absN = Math.abs((long) n);
+        while (absN != 0) {
+            if ((absN & 1) == 1) {
+                ans *= x;
+            }
+            absN = absN >> 1;
+                x *= x;
+        }
+        return n > 0 ? ans : 1 / ans;
+    }
+}
