4c031b95dbe95481571a789cd18ed9676706b655
jiapengliu613 <jiapengliu613@gmail.com>
2016-12-15 10:36:15 -0800

    Solution

---
+++ Algorithm/Contains Duplicate II.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if (nums == null || nums.length <= 1) {
+            return false;
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!map.containsKey(nums[i])) {
+                map.put(nums[i], i);
+            } else {
+                if (i - map.get(nums[i]) <= k) {
+                    return true;
+                } else {
+                    map.put(nums[i], i);
+                }
+            }
+        }
+        return false;
+    }
+}
