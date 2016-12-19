f277ef3dfff770c8d755f108bd7a85516165620f
jiapengliu613 <jiapengliu613@gmail.com>
2016-12-19 13:35:11 -0800

    Solution

---
+++ Algorithm/Search a 2D Matrix II.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int m = matrix.length, n = matrix[0].length;
+        int row = 0, col = n - 1;
+        while (row < m && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] < target) {
+                row++;
+            } else {
+                col--;
+            }
+        }
+        return false;
+    }
+}
