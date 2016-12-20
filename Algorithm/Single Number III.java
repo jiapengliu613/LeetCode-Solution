439537fdbe15ce63cc4400e14f81f8c6e2cfb896
jiapengliu613 <jiapengliu613@gmail.com>
2016-12-19 16:59:36 -0800

    Solution

---
+++ Algorithm/Single Number III.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int[] singleNumber(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return nums;
+        }
+        int diff = 0;
+        for (int num : nums) {
+            diff ^= num;
+        }
+        diff &= -diff;
+        int[] result = new int[2];
+        for (int num : nums) {
+            if ((num & diff) == 0) {
+                result[0] ^= num;
+            } else {
+                result[1] ^= num;
+            }
+        }
+        return result;
+    }
+}
