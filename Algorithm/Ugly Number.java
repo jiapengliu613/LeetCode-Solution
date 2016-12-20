8c6b9efb8ac2340e756914889a85429ff6ea16a2
jiapengliu613 <jiapengliu613@gmail.com>
2016-12-20 10:01:05 -0800

    Solution

---
+++ Algorithm/Ugly Number.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public boolean isUgly(int num) {
+        if (num <= 0) {
+            return false;
+        }
+        if (num == 1) {
+            return true;
+        }
+        while (num > 1 && num % 2 == 0) {
+            num = num / 2;
+        }
+        while (num > 1 && num % 3 == 0) {
+            num = num / 3;
+        }
+        while (num > 1 && num % 5 == 0) {
+            num = num / 5;
+        }
+        return num == 1;
+    }
+}
