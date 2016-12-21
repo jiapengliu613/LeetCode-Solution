2df645c398c617bf586b03a3e851f9897f2cb482
jiapengliu613 <jiapengliu613@gmail.com>
2016-12-21 10:45:23 -0800

    Solution

---
+++ Algorithm/Find Peak Element.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int findPeakElement(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        if (nums.length == 1) {
+            return 0;
+        }
+        int start = 0, end = nums.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+                return mid;
+            } else if (nums[mid] > nums[mid - 1] && nums[mid] < nums[mid + 1]) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+        return nums[start] > nums[end] ? start : end;
+    }
+}
