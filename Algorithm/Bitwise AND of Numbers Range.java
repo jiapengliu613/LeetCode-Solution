91dbdc13d97b2404eb58f2789f474199089e0c22
jiapengliu613 <jiapengliu613@gmail.com>
2017-01-04 18:16:20 -0800

    Solution

---
+++ Algorithm/Bitwise AND of Numbers Range.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int rangeBitwiseAnd(int m, int n) {
+        int move = 1;
+        while (m != n) {
+            m >>= 1;
+            n >>= 1;
+            move <<= 1;
+        }
+        return m * move;
+    }
+}
