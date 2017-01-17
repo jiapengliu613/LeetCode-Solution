fc767da0a117f7955a802f898fc40bd44d3ead45
jiapengliu613 <jiapengliu613@gmail.com>
2017-01-16 18:32:47 -0800

    Solution

---
+++ Algorithm/Permutation Sequence.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public String getPermutation(int n, int k) {
+        if (n == 0) {
+            return "";
+        }
+        
+        List<Integer> nums = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            nums.add(i);
+        }
+        int[] factorial = new int[n + 1];
+        factorial[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial[i] = i * factorial[i - 1];
+        }
+        k--;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            int index = k / factorial[n - i];
+            sb.append(nums.get(index));
+            nums.remove(nums.get(index));
+            k -= index * factorial[n - i];
+        }
+        return sb.toString();
+    }
+}
