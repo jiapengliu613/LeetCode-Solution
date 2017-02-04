8733e52d831a37be7050bef5a3320bf37ed1baff
jiapengliu613 <jiapengliu613@gmail.com>
2017-02-03 20:50:36 -0800

    Solution

---
+++ Algorithm/Two Sum III - Data structure design.java
@@ -0,0 +1,40 @@
+public class TwoSum {
+    private HashMap<Integer, Integer> map;
+    /** Initialize your data structure here. */
+    public TwoSum() {
+        map = new HashMap<Integer, Integer>();
+    }
+    
+    /** Add the number to an internal data structure.. */
+    public void add(int number) {
+        if (map.containsKey(number)) {
+            map.put(number, map.get(number) + 1);
+        } else {
+            map.put(number, 1);
+        }
+    }
+    
+    /** Find if there exists any pair of numbers which sum is equal to the value. */
+    public boolean find(int value) {
+        for (int a : map.keySet()) {
+            int target = value - a;
+            if (target == a) {
+                if (map.get(a) > 1) {
+                    return true;
+                }
+            } else {
+                if (map.containsKey(target)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+/**
+ * Your TwoSum object will be instantiated and called as such:
+ * TwoSum obj = new TwoSum();
+ * obj.add(number);
+ * boolean param_2 = obj.find(value);
+ */
