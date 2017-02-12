dcca00641901a9e34fb4f37a30cb3ef07b45df04
jiapengliu613 <jiapengliu613@gmail.com>
2017-02-12 14:54:44 -0800

    Update

--- Algorithm/Maximum Product Subarray.java
+++ Algorithm/Maximum Product Subarray.java
@@ -3,6 +3,20 @@ public class Solution {
         if (nums == null || nums.length == 0) {
             return 0;
         }
+        int result = nums[0], min = nums[0], max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            // min max represent the minValue and maxValue that end up with nums[i], so it can decide whether to use the previous i - 1 number or not
+            int a = min * nums[i];
+            int b = max * nums[i];
+            min = Math.min(nums[i], Math.min(a, b));
+            max = Math.max(nums[i], Math.max(a, b));
+            result = Math.max(min, max);
+        }
+        return result;
+        
+        
+        
+        /*
         int globalMax = nums[0];
         int localMax = nums[0], localMin = nums[0];
         for (int i = 1; i < nums.length; i++) {
@@ -16,6 +30,7 @@ public class Solution {
             globalMax = Math.max(globalMax, localMax);
         }
         return globalMax;
+        */
         
     }
 }
