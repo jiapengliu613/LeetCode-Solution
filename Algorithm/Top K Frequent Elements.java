e6b65152a8214c9dc2b2185d7e843d3b342f0781
jiapengliu613 <jiapengliu613@gmail.com>
2017-02-12 14:14:13 -0800

    Update

--- Algorithm/Top K Frequent Elements.java
+++ Algorithm/Top K Frequent Elements.java
@@ -4,6 +4,71 @@ public class Solution {
         if (nums == null || nums.length == 0 || k == 0) {
             return result;
         }
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        List<Integer>[] buckets = new List[n + 1];
+        for (int key : map.keySet()) {
+            int freq = map.get(key);
+            if (buckets[freq] == null) {
+                buckets[freq] = new ArrayList<>();
+            }
+            buckets[freq].add(key);
+        }
+        for (int i = n; i >= 0; i--) {
+            if (result.size() >= k) {
+                break;
+            }
+            if (buckets[i] == null) {
+                continue;
+            }
+            result.addAll(buckets[i]);
+        }
+        return result;
+        
+        
+        /* PQ O(nlog(n)) Solution
+        List<Integer> result = new ArrayList<>();
+        if (nums == null || nums.length == 0 || k == 0) {
+            return result;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>(k, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer a, Integer b) {
+                return map.get(b) - map.get(a);
+            }
+        });
+        for (int num : map.keySet()) {
+            pq.offer(num);
+        }
+        for (int i = 0; i < k; i++) {
+            if (pq.isEmpty()) {
+                break;
+            }
+            result.add(pq.poll());
+        }
+        return result;
+        */
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        /*
+        List<Integer> result = new ArrayList<>();
+        if (nums == null || nums.length == 0 || k == 0) {
+            return result;
+        }
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int num : nums) {
             if (!map.containsKey(num)) {
@@ -29,5 +94,6 @@ public class Solution {
             result.add((Integer)pq.poll().getKey());
         }
         return result;
+        */
     }
 }
