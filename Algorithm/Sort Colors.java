e4b4796f7f906f843fc24d3ae824f8c515b7d1fd
jiapengliu613 <jiapengliu613@gmail.com>
2017-02-12 17:05:45 -0800

    Update

--- Algorithm/Sort Colors.java
+++ Algorithm/Sort Colors.java
@@ -1,8 +1,34 @@
 public class Solution {
+    
+    
+    
     public void sortColors(int[] nums) {
         if (nums == null || nums.length < 2) {
             return ;
         }
+        
+        
+        int n = nums.length;
+        int left = 0, right = nums.length - 1;
+        for (int i = left; i <= right;) {
+            if (nums[i] == 0) {
+                swap(nums, left, i);
+                left++;
+                i++;
+                //System.out.println("im here");
+            } else if (nums[i] == 2) {
+                swap(nums, right, i);
+                right--;
+            } else {
+                i++;
+            }
+        }
+        
+        
+    
+        
+        
+        /*
         int low = 0, high = nums.length - 1;
         for (int i = low; i <= high;) {
             if (nums[i] == 0) {
@@ -17,6 +43,7 @@ public class Solution {
             }
         }
         return ;
+        */
     }
     private void swap(int[] nums, int i, int j) {
         int tmp = nums[i];
