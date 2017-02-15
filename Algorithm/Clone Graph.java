9f32e4bb973af415f7767ad58b10a7efd47352c6
jiapengliu613 <jiapengliu613@gmail.com>
2017-02-14 23:13:40 -0800

    BFS solution

--- Algorithm/Clone Graph.java
+++ Algorithm/Clone Graph.java
@@ -8,12 +8,56 @@
  */
 public class Solution {
     public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        //BFS
         if (node == null) {
             return null;
         }
         if (node.neighbors == null || node.neighbors.size() == 0) {
             return new UndirectedGraphNode(node.label);
         }
+        Map<Integer, UndirectedGraphNode> map = new HashMap<>();
+        Deque<UndirectedGraphNode> queue = new ArrayDeque<>();
+        UndirectedGraphNode start = new UndirectedGraphNode(node.label);
+        map.put(node.label, start);
+        queue.offer(node);
+        while (!queue.isEmpty()) {
+            UndirectedGraphNode current = queue.poll();
+            for (UndirectedGraphNode neighbor : current.neighbors) {
+                if (!map.containsKey(neighbor.label)) {
+                    UndirectedGraphNode newNode = new UndirectedGraphNode(neighbor.label);
+                    map.put(neighbor.label, newNode);
+                    queue.offer(neighbor);
+                }
+                map.get(current.label).neighbors.add(map.get(neighbor.label));
+            }
+        }
+        return start;
+        
+  
+        
+        /*
+        Map<Integer, UndirectedGraphNode> map = new HashMap<>();
+        Deque<UndirectedGraphNode> queue = new ArrayDeque<>();
+        UndirectedGraphNode newNode = new UndirectedGraphNode(node.label);
+        map.put(node.label, newNode);
+        queue.offer(node);
+        while (!queue.isEmpty()) {
+            UndirectedGraphNode current = queue.poll();
+            for (UndirectedGraphNode neighbor : current.neighbors) {
+                if (!map.containsKey(neighbor.label)) {
+                    UndirectedGraphNode next = new UndirectedGraphNode(neighbor.label);
+                    map.put(neighbor.label, next);
+                    queue.offer(neighbor);
+                    
+                    //continue;
+                }
+                map.get(current.label).neighbors.add(map.get(neighbor.label));
+                //map.get(current.label).neighbors.add(next);
+            }
+        }
+        return newNode;
+       /*
+        
         Map<Integer, UndirectedGraphNode> map = new HashMap<>();
         return helper(node, map);
         
@@ -28,5 +72,6 @@ public class Solution {
             cloned.neighbors.add(helper(neighbor, map));
         }
         return cloned;
+        */
     }
 }
