eac598d3e37ebd72226faa16739b9c5938f33932
jiapengliu613 <jiapengliu613@gmail.com>
2017-02-22 13:00:30 -0800

    dp Solution

---
+++ Algorithm/Ones and Zeroes.java
@@ -0,0 +1,33 @@
+public class Solution {
+    public int findMaxForm(String[] strs, int m, int n) {
+        if (strs == null || strs.length == 0 || (m == 0 && n == 0)) {
+            return 0;
+        }
+        int[][] dp = new int[m + 1][n + 1];
+        for (String str : strs) {
+            int[] count = findCount(str);
+            for (int i = m; i >= 1; i--) {
+                for (int j = n; j >= 1; j--) {
+ 
+                    //dp[i][j] = Math.max(dp[i - 1][j], Math.max(dp[i][j - 1], dp[i - 1][j - 1]));
+                    
+                    if (i >= count[0] && j >= count[1]) {
+                        dp[i][j] = Math.max(dp[i - count[0]][j - count[1]] + 1, dp[i][j]);
+                    }
+                }
+            }
+        }
+        return dp[m][n];
+    }
+    private int[] findCount(String s) {
+        int[] result = new int[2];
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '0') {
+                result[0]++;
+            } else {
+                result[1]++;
+            }
+        }
+        return result;
+    }
+}
