929c2076b01844aa6866d3ce10dcb7a210e09cfa
jiapengliu613 <jiapengliu613@gmail.com>
2017-02-22 17:04:02 -0800

    Solution

--- Algorithm/Ones and Zeroes.java
+++ Algorithm/Ones and Zeroes.java
@@ -6,13 +6,13 @@ public class Solution {
         int[][] dp = new int[m + 1][n + 1];
         for (String str : strs) {
             int[] count = findCount(str);
-            for (int i = m; i >= 1; i--) {
-                for (int j = n; j >= 1; j--) {
+            for (int i = m; i >= 0; i--) {
+                for (int j = n; j >= 0; j--) {
  
                     //dp[i][j] = Math.max(dp[i - 1][j], Math.max(dp[i][j - 1], dp[i - 1][j - 1]));
                     
                     if (i >= count[0] && j >= count[1]) {
-                        dp[i][j] = Math.max(dp[i - count[0]][j - count[1]] + 1, dp[i][j]);
+                    dp[i][j] = Math.max(dp[i - count[0]][j - count[1]] + 1, dp[i][j]);
                     }
                 }
             }
